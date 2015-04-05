7fbfebdc55856bfa2340c7158a99d60c027e2b11
luke <luke.brooks42@gmail.com>
2015-04-05 16:49:28 +0100

    capabilities.py now checks its environment properly

--- scripts/capabilities.py
+++ scripts/capabilities.py
@@ -1,4 +1,4 @@
 if 'init' in dir(): provides="command:cap:Simple response to IRCv3 capabilities requests."
-else:
+elif "client" in dir() and client.nick:
     client.broadcast(client.nick, ": This server doesn't currently implement the IRC v3 capabilities model.")
 
