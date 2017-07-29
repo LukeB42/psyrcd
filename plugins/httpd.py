56991cdfce69ece00596dfeab2151fd30f844896
Luke Brooks <luke.brooks42@gmail.com>
2017-07-29 12:25:04 +0100

    Removes/fixes the closure in the example httpd plugin.

--- plugins/httpd.py
+++ plugins/httpd.py
@@ -6,21 +6,23 @@ from tornado.wsgi import WSGIContainer
 from tornado.web import Application, FallbackHandler
 from tornado.platform.asyncio import AsyncIOMainLoop
 
+ircd = None
+
 app  = Flask("httpd")
+@app.route("/")
+def index():
+    global ircd
+    response = Response(mimetype="text/html")
+    response.data = "I have %i clients." % len(ircd.clients)
+
+    return response
 
 def httpd(ctx):
     return "Running."
 
 def __init__(ctx):
-
-    @app.route("/")
-    def index():
-        response = Response(mimetype="text/html")
-
-        response.data = "I have %i clients." % len(ctx.server.clients)
-
-        return response
-
+    global ircd
+    ircd = ctx.server
     container   = WSGIContainer(app)
     application = Application([
         (".*", FallbackHandler, {"fallback": container})    
